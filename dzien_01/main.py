7883eff241274903f35e620479ece66173d6c4c3
Łukasz Prokulski <prokulski@gmail.com>
2024-10-14 11:55:22 +0200

    dzien 1

--- dzien_01/main.py
+++ dzien_01/main.py
@@ -1,6 +1,43 @@
 # komentarz - od znaku # do końca linii
 
 # wypisanie tekstu na ekranie
-print("jakiś tekst")
+# print("jakiś tekst")
+# print("inny tekst")
 
-print('inny tekst')
+
+# w Java byłoby to tak:
+# class Napis {
+#     public static void main(String[] arg) {
+#         System.out.println("jakiś tekst");
+#     }
+# }
+
+
+# standard PEP 8
+# https://analityk.edu.pl/jak-pisac-kod-w-python-aby-byl-czytelny-pep8/
+
+
+### ZMIENNE
+# zmienna = 123
+# print("zmienna") # wyświetla napis "zmienna"
+# print(zmienna) # wyświetla wartość zmiennej o nazwie zmienna
+# print(type(zmienna)) # wyświetla typ zmiennej
+
+# zmienna = "Ala ma kota"
+# print(zmienna)
+# print(type(zmienna))
+
+# zmienna = 3.1415926
+# print(zmienna)
+# print(type(zmienna))
+
+zmienna_int = 1
+zmienna_float = 2.3
+zmienna_str = "abc"
+
+# print(zmienna_int + zmienna_float) # to się uda
+# print(zmienna_int + zmienna_str) # to się nei uda
+
+# print(zmienna_int * 5)
+# print(zmienna_float * 5)
+# print(zmienna_str * 5) # ciekawostka - można mnożyć stringi
