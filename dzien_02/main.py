4c2af8cb8e63ab14d0e211f49680ccc6d94d862c
Łukasz Prokulski <prokulski@gmail.com>
2024-10-15 09:25:37 +0200

    dzień 2

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -1,3 +1,26 @@
 # jak zakończyć pętlę przed czasem?
+
+# for i in range(100):
+#     print(i**2)
+#     if i**2 > 8000:
+#         break
+# print(f"Po zakończeniu pętli {i=}")
+
+
 # jak ominąć iterację?
+# for i in range(-10, 10):
+#     print(i)
+#     if i == 0:
+#         print("przechodzę dalej")
+#         continue
+#     print("dzielenie", 1/i)
+
+    
+    
+#### ZADANIE 10
+
+# Przejdź w pętli po liczbach od 1 do 100 i wydrukuj tylko nieparzyste. Użyj konstrukcji z continue.
+
+
+
 # jak sprawdzić czy pętla przeszła wszystkie iteracje?
