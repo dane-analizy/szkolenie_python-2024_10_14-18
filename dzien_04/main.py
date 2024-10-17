f4ed735f3ba698a55de6309125583e55e9e5695f
Łukasz Prokulski <prokulski@gmail.com>
2024-10-17 11:01:06 +0200

    dzien 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -216,33 +216,39 @@
 
 ### DRY - Don't Repeat Yourself
 
-def check_parameter(p):
-    if not isinstance(p, (int, float)):
-        return False
-    if p < 0:
-        return False
-    return True
+# def check_parameter(p):
+#     if not isinstance(p, (int, float)):
+#         return False
+#     if p < 0:
+#         return False
+#     return True
     
 
-def bmi(waga_kg, wzrost_m):
-    # sprawdzamy czy waga jest ok
-    if not check_parameter(waga_kg):
-        print("Waga musi być liczbą dodatnią!")
-        return None
+# def bmi(waga_kg, wzrost_m):
+#     # sprawdzamy czy waga jest ok
+#     if not check_parameter(waga_kg):
+#         print("Waga musi być liczbą dodatnią!")
+#         return None
 
-    # sprawdzamy czy wzrost jest ok
-    if not check_parameter(wzrost_m):
-        print("Wzrost musi być liczbą dodatnią!")
-        return None
+#     # sprawdzamy czy wzrost jest ok
+#     if not check_parameter(wzrost_m):
+#         print("Wzrost musi być liczbą dodatnią!")
+#         return None
 
-    # możemy liczyć wynik
-    wynik = round(waga_kg / (wzrost_m**2), 2)
-    return wynik
+#     # możemy liczyć wynik
+#     wynik = round(waga_kg / (wzrost_m**2), 2)
+#     return wynik
 
 
-print(bmi(90, 1.80))
-print(bmi("alamakota", 1.80))
-print(bmi(78, -1.80))
+# print(bmi(90, 1.80))
+# print(bmi("alamakota", 1.80))
+# print(bmi(78, -1.80))
+
+
+# moduły funkcji
+
+
+
 
 
 # wyjątki
