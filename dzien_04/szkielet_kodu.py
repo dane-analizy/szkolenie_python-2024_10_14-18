caef077c92398302d053acdd3a5c7fec305781c2
Łukasz Prokulski <prokulski@gmail.com>
2024-10-17 12:10:36 +0200

    dzien 4

---
+++ dzien_04/szkielet_kodu.py
@@ -0,0 +1,10 @@
+import ... # funkcje z pakietów
+
+def funkcja(): # dedykowane funkcje, których nie warto wstawiać w pakiet
+    pass
+
+def main():
+    pass # tutaj cały przebieg programu
+
+if __name__ == '__main__':
+    main()
