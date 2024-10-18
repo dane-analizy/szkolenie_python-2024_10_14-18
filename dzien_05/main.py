2349eced4ba1b2325229c2d5b5b91c66fa717a75
Łukasz Prokulski <prokulski@gmail.com>
2024-10-18 08:58:28 +0200

    dzien 4

--- dzien_05/main.py
+++ dzien_05/main.py
@@ -14,3 +14,23 @@ create table players (
         weight numeric not null
 );
 """
+
+"""
+insert into players (first_name,last_name,height,weight)  
+values ('Jan', 'Kowalski', 1.84, 85);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Marian', 'Nowak', 1.90, 50);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Zdzisław', 'Dyrman', 1.73 ,74);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Zenon', 'Brzęczyk', 1.64, 95);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Chuck','Norris', 1.82, 78);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Krzysztof','Jarzyna', 1.68, 70);
+"""
